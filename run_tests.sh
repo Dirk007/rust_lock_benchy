49bf685b335548312f1d552e2c55b5911d2936e9
Dirk Faust <dif@crosscan.com>
2021-10-20 10:38:31 +0200

    Use more comnon format

--- run_tests.sh
+++ run_tests.sh
@@ -10,6 +10,7 @@ do
     echo "- ${WHAT}"
     cargo build --release --features="$WHAT" --no-default-features 1>/dev/null 2>&1
     BENCH_VERSION="$(target/release/test_mux --version)"
+    rm -f test_mux_$WHAT
     mv target/release/test_mux test_mux_$WHAT
 done
 
@@ -17,15 +18,18 @@ echo "Collecting informations..."
 echo "* BENCH VERSION: ${BENCH_VERSION}"
 echo "* CPU:"
 CPU="$(lscpu 2>/dev/null || true)"
-if [ -z $CPU ]; then
+if [ -z "$CPU" ]; then
     CPU="$(sysctl -n machdep.cpu.brand_string  2>/dev/null || true)"
+    CPU_SPEED="@$(echo $CPU | cut -d"@" -f2 | tr -d " ")"
+else
+    CPU_SPEED="@$(lscpu | grep "CPU max" | cut -d":" -f2 | tr -d " ")"
 fi
 echo $CPU
 
 echo "* System:"
 SYSTEM="$(uname -mrs || true)"
 
-TARGET="$(echo "$SYSTEM $CPU" | tr " " "_" | tr "/" "_" | tr "\\" "_")"
+TARGET="$(echo "$SYSTEM $(nproc || true) ${CPU_SPEED}" | tr " " "_" | tr "/" "_")"
 echo "$TARGET"
 
 TARGET="benchmarks/${TARGET}_bench.txt"
